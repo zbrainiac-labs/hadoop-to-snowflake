f43092de20ac9af3fb62577d825463dae193c741
sfc-gh-mdaeppen <marcel.daeppen@snowflake.com>
2026-05-28 15:45:03 +0200

    Fix pre_deploy.sql template vars to use manifest variables

--- pre_deploy.sql
+++ pre_deploy.sql
@@ -1,9 +1,9 @@
-CREATE DATABASE IF NOT EXISTS {{ sf_db }}
+CREATE DATABASE IF NOT EXISTS {{ domain }}_{{ env }}
     COMMENT = 'HAM domain - Hadoop migration showcase';
 
-CREATE SCHEMA IF NOT EXISTS {{ sf_db }}.HAM_DCM
+CREATE SCHEMA IF NOT EXISTS {{ domain }}_{{ env }}.HAM_DCM
     COMMENT = 'DCM project schema for Hadoop migration';
-CREATE SCHEMA IF NOT EXISTS {{ sf_db }}.HAM_RAW_V001
+CREATE SCHEMA IF NOT EXISTS {{ domain }}_{{ env }}.{{ domain }}_{{ maturity }}_V{{ version }}
     COMMENT = 'RAW ingestion layer - migrated Hive/HMS tables';
 
-CREATE DCM PROJECT IF NOT EXISTS {{ sf_db }}.HAM_DCM.HAM_DCM_PROJECT;
+CREATE DCM PROJECT IF NOT EXISTS {{ domain }}_{{ env }}.HAM_DCM.HAM_DCM_PROJECT;
